7dbada665f47a6c8cc8e4dc1d51150691e48f5b6
Jamie Lord <jamie@lord.technology>
2020-02-23 13:45:38 +0000

    Include source in item and begun tidying up returned content string

--- Rss.Server/Services/ItemService.cs
+++ Rss.Server/Services/ItemService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CodeHollow.FeedReader;
 using Google.Protobuf.WellKnownTypes;
@@ -10,28 +13,37 @@ namespace Rss.Server.Services
 {
     public class ItemService : Items.ItemsBase
     {
+
+        private readonly string[] Sources = { "https://www.theguardian.com/world/rss", "http://feeds.arstechnica.com/arstechnica/index", "https://hackaday.com/blog/feed/" };
+
         public override async Task<Reply> GetItems(Empty request, ServerCallContext context)
         {
-            Feed feed = await FeedReader.ReadAsync("https://www.theguardian.com/world/rss");
-            var reply = new Reply();
-            foreach (FeedItem feedItem in feed.Items)
+            var items = new List<Item>();
+            foreach (var source in Sources)
             {
-                var item = new Item
+                Feed feed = await FeedReader.ReadAsync(source);
+                foreach (FeedItem feedItem in feed.Items)
                 {
-                    Link = feedItem.Link,
-                    Title = feedItem.Title,
-                    PublishDate = feedItem.PublishingDate.HasValue ? feedItem.PublishingDate.Value.ToTimestamp() : DateTime.Now.ToTimestamp()
-                };
-                if (!string.IsNullOrWhiteSpace(feedItem.Author))
-                {
-                    item.Author = feedItem.Author;
+                    var item = new Item
+                    {
+                        SourceTitle = feed.Title,
+                        Link = feedItem.Link,
+                        Title = feedItem.Title,
+                        PublishDate = feedItem.PublishingDate.HasValue ? feedItem.PublishingDate.Value.ToTimestamp() : DateTime.Now.ToTimestamp()
+                    };
+                    if (!string.IsNullOrWhiteSpace(feedItem.Author))
+                    {
+                        item.Author = feedItem.Author;
+                    }
+                    if (!string.IsNullOrWhiteSpace(feedItem.Content))
+                    {
+                        item.Content = feedItem.Content;
+                    }
+                    items.Add(item);
                 }
-                if (!string.IsNullOrWhiteSpace(feedItem.Content))
-                {
-                    item.Content = feedItem.Content;
-                }
-                reply.Items.Add(item);
             }
+            var reply = new Reply();
+            reply.Items.Add(items.OrderByDescending(x => x.PublishDate));
             return reply;
         }
 
@@ -48,7 +60,7 @@ namespace Rss.Server.Services
                     SiteName = smArticle.SiteName,
                     Language = smArticle.Language,
                     Excerpt = smArticle.Excerpt,
-                    Content = smArticle.TextContent,
+                    Content = Regex.Replace(smArticle.TextContent, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline),
                     FeaturedImage = smArticle.FeaturedImage,
                     ByLine = smArticle.Byline,
                     Title = smArticle.Title
